5dc89b0db9a71eb068134cb9313c01c1b7b0bc5b
Zheng, Zhaoqiong <zhaoqiong.zheng@intel.com>
2025-04-29 03:17:24 +0000

    add sycl cpp extension

--- setup.py
+++ setup.py
@@ -11,61 +11,37 @@ from setuptools import find_packages, setup
 
 from torch.utils.cpp_extension import (
     CppExtension,
-    CUDAExtension,
+    SyclExtension,
     BuildExtension,
-    CUDA_HOME,
 )
 
 library_name = "extension_cpp"
 
-if torch.__version__ >= "2.6.0":
-    py_limited_api = True
-else:
-    py_limited_api = False
-
 
 def get_extensions():
-    debug_mode = os.getenv("DEBUG", "0") == "1"
-    use_cuda = os.getenv("USE_CUDA", "1") == "1"
-    if debug_mode:
-        print("Compiling in debug mode")
 
-    use_cuda = use_cuda and torch.cuda.is_available() and CUDA_HOME is not None
-    extension = CUDAExtension if use_cuda else CppExtension
-
-    extra_link_args = []
+    use_sycl = torch.xpu.is_available()
+    extension = SyclExtension if use_sycl else CppExtension
     extra_compile_args = {
-        "cxx": [
-            "-O3" if not debug_mode else "-O0",
-            "-fdiagnostics-color=always",
-            "-DPy_LIMITED_API=0x03090000",  # min CPython version 3.9
-        ],
-        "nvcc": [
-            "-O3" if not debug_mode else "-O0",
-        ],
+        "cxx": ["-g" ],
+        "sycl": ["-O2"],
     }
-    if debug_mode:
-        extra_compile_args["cxx"].append("-g")
-        extra_compile_args["nvcc"].append("-g")
-        extra_link_args.extend(["-O0", "-g"])
 
     this_dir = os.path.dirname(os.path.curdir)
     extensions_dir = os.path.join(this_dir, library_name, "csrc")
     sources = list(glob.glob(os.path.join(extensions_dir, "*.cpp")))
 
-    extensions_cuda_dir = os.path.join(extensions_dir, "cuda")
-    cuda_sources = list(glob.glob(os.path.join(extensions_cuda_dir, "*.cu")))
+    extensions_sycl_dir = os.path.join(extensions_dir, "sycl")
+    sycl_sources = list(glob.glob(os.path.join(extensions_sycl_dir, "*.sycl")))
 
-    if use_cuda:
-        sources += cuda_sources
+    if use_sycl:
+        sources += sycl_sources
 
     ext_modules = [
-        extension(
-            f"{library_name}._C",
-            sources,
+        SyclExtension(
+            name=f"{library_name}._C",
+            sources=sources,
             extra_compile_args=extra_compile_args,
-            extra_link_args=extra_link_args,
-            py_limited_api=py_limited_api,
         )
     ]
 
@@ -74,14 +50,8 @@ def get_extensions():
 
 setup(
     name=library_name,
-    version="0.0.1",
     packages=find_packages(),
     ext_modules=get_extensions(),
-    install_requires=["torch"],
-    description="Example of PyTorch C++ and CUDA extensions",
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
-    url="https://github.com/pytorch/extension-cpp",
-    cmdclass={"build_ext": BuildExtension},
-    options={"bdist_wheel": {"py_limited_api": "cp39"}} if py_limited_api else {},
+    description="Example of PyTorch C++ and SYCL extensions",
+    cmdclass={"build_ext": BuildExtension.with_options(use_xpu=True)},
 )
--- test/test_extension.py
+++ test/test_extension.py
@@ -37,9 +37,9 @@ class TestMyMulAdd(TestCase):
     def test_correctness_cpu(self):
         self._test_correctness("cpu")
 
-    @unittest.skipIf(not torch.cuda.is_available(), "requires cuda")
-    def test_correctness_cuda(self):
-        self._test_correctness("cuda")
+    @unittest.skipIf(not torch.xpu.is_available(), "requires xpu")
+    def test_correctness_xpu(self):
+        self._test_correctness("xpu")
 
     def _test_gradients(self, device):
         samples = self.sample_inputs(device, requires_grad=True)
@@ -57,9 +57,9 @@ class TestMyMulAdd(TestCase):
     def test_gradients_cpu(self):
         self._test_gradients("cpu")
 
-    @unittest.skipIf(not torch.cuda.is_available(), "requires cuda")
-    def test_gradients_cuda(self):
-        self._test_gradients("cuda")
+    @unittest.skipIf(not torch.xpu.is_available(), "requires xpu")
+    def test_gradients_xpu(self):
+        self._test_gradients("xpu")
 
     def _opcheck(self, device):
         # Use opcheck to check for incorrect usage of operator registration APIs
@@ -71,46 +71,9 @@ class TestMyMulAdd(TestCase):
     def test_opcheck_cpu(self):
         self._opcheck("cpu")
 
-    @unittest.skipIf(not torch.cuda.is_available(), "requires cuda")
-    def test_opcheck_cuda(self):
-        self._opcheck("cuda")
-
-
-class TestMyAddOut(TestCase):
-    def sample_inputs(self, device, *, requires_grad=False):
-        def make_tensor(*size):
-            return torch.randn(size, device=device, requires_grad=requires_grad)
-
-        def make_nondiff_tensor(*size):
-            return torch.randn(size, device=device, requires_grad=False)
-
-        return [
-            [make_tensor(3), make_tensor(3), make_tensor(3)],
-            [make_tensor(20), make_tensor(20), make_tensor(20)],
-        ]
-
-    def _test_correctness(self, device):
-        samples = self.sample_inputs(device)
-        for args in samples:
-            result = args[-1]
-            extension_cpp.ops.myadd_out(*args)
-            expected = torch.add(*args[:2])
-            torch.testing.assert_close(result, expected)
-
-    def _opcheck(self, device):
-        # Use opcheck to check for incorrect usage of operator registration APIs
-        samples = self.sample_inputs(device, requires_grad=True)
-        samples.extend(self.sample_inputs(device, requires_grad=False))
-        for args in samples:
-            opcheck(torch.ops.extension_cpp.myadd_out.default, args)
-
-    def test_opcheck_cpu(self):
-        self._opcheck("cpu")
-
-    @unittest.skipIf(not torch.cuda.is_available(), "requires cuda")
-    def test_opcheck_cuda(self):
-        self._opcheck("cuda")
-
+    @unittest.skipIf(not torch.xpu.is_available(), "requires xpu")
+    def test_opcheck_xpu(self):
+        self._opcheck("xpu")
 
 if __name__ == "__main__":
     unittest.main()
